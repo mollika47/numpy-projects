8176f332c75d0c9fc3f4a994bf435500f1ecfb1e
Mollika Barman <mollika47@users.noreply.github.com>
2026-08-31 21:20:35 +0600

    add subjects and functions for highest, lowest and average marks

--- student-marks-analyzer/student-marks-analyzer.py
+++ student-marks-analyzer/student-marks-analyzer.py
@@ -1,4 +1,5 @@
 import numpy as np
+subjects = np.array(["Math", "English", "Science", "Python"])
 marks = np.array([
 #sub[mat, eng, sci, pyt]
     [56, 60, 85, 76], #student 1
@@ -7,3 +8,34 @@ marks = np.array([
     [74, 83, 94, 69], #student 4
     [83, 78, 92, 87]  #student 5
 ])
+
+def sub_wise_highest_marks():
+    print("Subject-wise highest marks:")
+    highest_marks = np.max(marks, axis=0)
+
+    for sub, mark in zip(subjects, highest_marks):
+        print(sub,"\b:", mark)
+    print("\n")
+
+def sub_wise_lowest_marks():
+    print("Subject-wise lowest marks:")
+    lowest_marks = np.min(marks, axis=0)
+
+    for sub, mark in zip(subjects, lowest_marks):
+        print(sub,"\b:", mark)
+    print("\n")
+
+def sub_wise_average_marks():
+    print("Subject-wise average marks:")
+    avg_marks = np.mean(marks, axis=0)
+
+    for sub, mark in zip(subjects, avg_marks):
+        print(sub,"\b:", mark)
+    print("\n")
+
+sub_wise_highest_marks()
+sub_wise_lowest_marks()
+sub_wise_average_marks()
+
+
+
