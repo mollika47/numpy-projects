48e23ed2d1fc0da39b43d28100f38291ebb53a9f
Mollika Barman <mollika47@users.noreply.github.com>
2026-08-31 16:30:50 +0600

    create marks array

---
+++ student-marks-analyzer/student-marks-analyzer.py
@@ -0,0 +1,9 @@
+import numpy as np
+marks = np.array([
+#sub[mat, eng, sci, pyt]
+    [56, 60, 85, 76], #student 1
+    [68, 56, 72, 59], #student 2
+    [86, 64, 73, 56], #student 3
+    [74, 83, 94, 69], #student 4
+    [83, 78, 92, 87]  #student 5
+])
