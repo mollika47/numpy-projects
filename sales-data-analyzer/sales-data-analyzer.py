85ac2cba0fc958d39de55eb09adb3fe87a782635
Mollika Barman <mollika47@users.noreply.github.com>
2026-09-01 19:07:58 +0600

    add product_analysis() function

--- sales-data-analyzer/sales-data-analyzer.py
+++ sales-data-analyzer/sales-data-analyzer.py
@@ -10,12 +10,34 @@ sales = np.array([
 
 def products_info():
     total_products = np.size(products)
-    print("Total Products: ", total_products)
+    print("Total Products:", total_products)
 
     total_months = np.size(sales, axis=0)
-    print("Total Months: ", total_months)
+    print("Total Months:", total_months)
 
-    print("Array Shape: ", np.shape(sales))
+    print("Array Shape:", np.shape(sales))
 
+def product_analysis():
+    print("\nTotal Sales of Each Product:")
+    total = np.sum(sales, axis=0)
+    for p, t in zip(products, total):
+        print(p,"\b:", t)
 
+    print("\nAverage Sales of Each Product:")
+    avg = np.mean(sales, axis=0)
+    for p, a in zip(products, avg):
+        print(p,"\b:", a)
+
+    print("\nHighest Sales of Each Product:")
+    highest = np.max(sales, axis=0)
+    for p, h in zip(products, highest):
+        print(p,"\b:", h)
+
+    print("\nLowest Sales of Each Product:")
+    lowest = np.min(sales, axis=0)
+    for p, l in zip(products, lowest):
+        print(p,"\b:", l)
+
+print("-------- Sales Data Analyzer --------\n")
 products_info()
+product_analysis()
