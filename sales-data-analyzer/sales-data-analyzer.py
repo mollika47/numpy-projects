55a2335c194772936100e637e0975dd0482b08dd
Mollika Barman <mollika47@users.noreply.github.com>
2026-09-01 17:22:12 +0600

    project-2 initial commit

---
+++ sales-data-analyzer/sales-data-analyzer.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+products = np.array(["Laptop", "Phone", "Tablet", "Headphone"])
+sales = np.array([
+    [12, 15, 10, 18],  # January
+    [14, 18, 13, 20],  # February
+    [10, 16, 15, 17],  # March
+    [18, 20, 17, 22]   # April
+])
+
