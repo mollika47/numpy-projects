2d036233c32932d92b8faad9a9eb9ed21fe0f844
Mollika Barman <mollika47@users.noreply.github.com>
2026-09-01 14:20:05 +0600

    update student_avg() function

--- student-marks-analyzer/student-marks-analyzer.py
+++ student-marks-analyzer/student-marks-analyzer.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 subjects = np.array(["Math", "English", "Science", "Python"])
-students = np.array(["Henry", "Isabella", "Millie", "Joe"])
+students = np.array(["Henry", "Isabella", "Millie", "Joe", "Zendaya"])
 marks = np.array([
     #sub →
     [56, 60, 85, 76], #students ↓
@@ -10,14 +10,6 @@ marks = np.array([
     [74, 83, 94, 69],
     [83, 78, 92, 87]
 ])
-
-print("---------- STUDENT MARKS ANALYZER ----------\n")
-
-total_st = np.size(students)
-print("Total Students: ",total_st)
-
-total_sub = np.size(subjects)
-print("Total Subjects: ",total_sub)
 
 def sub_wise_highest_marks():
     print("\nSubject-wise highest marks:")
@@ -40,9 +32,31 @@ def sub_wise_average_marks():
     for sub, mark in zip(subjects, avg_marks):
         print(sub,"\b:", mark)
 
+def student_avg():
+    print("\nStudent average:")
+    s_avg = np.mean(marks, axis=1)
+    for s, mark in zip(students, s_avg):
+        print(s,"\b:", mark)
+
+    best_index = np.argmax(s_avg)
+    worst_index = np.argmin(s_avg)
+    print("\nBest Scorer: ", students[best_index], s_avg[best_index])
+    print("Worst Scorer: ", students[worst_index], s_avg[worst_index])
+
+print("---------- STUDENT MARKS ANALYZER ----------\n")
+
+total_st = np.size(students)
+print("Total Students: ",total_st)
+
+total_sub = np.size(subjects)
+print("Total Subjects: ",total_sub)
+
 sub_wise_highest_marks()
 sub_wise_lowest_marks()
 sub_wise_average_marks()
+student_avg()
+
+
 
 
 
