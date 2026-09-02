c4c91af5c6e1335954283c6a5aa381bc5f534192
Mollika Barman <mollika47@users.noreply.github.com>
2026-09-02 19:20:32 +0600

    initial commit

---
+++ employee-salary-analyzer/employee-salary-analyzer.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+employees = np.array(["Henry", "Joe", "Kiara", "Sarah", "Jayson", "Lucia", "Arthur", "Thomas"])
+months = np.array(["January", "February", "March", "April"])
+salaries = np.array([
+    [40000, 40000, 42000, 45000],
+    [30000, 30500, 31000, 33000],
+    [40000, 40000, 40000, 42000],
+    [50000, 55000, 55000, 60000],
+    [20000, 26000, 26500, 29000],
+    [15000, 15500, 15500, 20000],
+    [30000, 31000, 31000, 35000],
+    [20000, 20000, 20000, 25000]
+])
