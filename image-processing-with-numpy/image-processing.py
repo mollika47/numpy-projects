1d71e570103a9cd440057ae4af926e6c9a3fe065
Mollika Barman <mollika47@users.noreply.github.com>
2026-09-03 20:37:54 +0600

    inspect image properties

--- image-processing-with-numpy/image-processing.py
+++ image-processing-with-numpy/image-processing.py
@@ -2,4 +2,12 @@ from PIL import Image
 import numpy as np
 
 img = np.array(Image.open("image/GTA VI(original).jpg"))
-print(img)
+
+def inspect_image():
+    print("Image Properties:")
+    h, w, c = img.shape
+    print(f"Dimension: {img.ndim}D")
+    print(f"Resolution: {w}x{h}")
+    print(f"Channels: {c}")
+
+inspect_image()
