cddb1131b7fdc0bd9cacfbbaf9959f3f1721ebc6
Mollika Barman <mollika47@users.noreply.github.com>
2026-09-03 23:48:54 +0600

    add pixel_analysis()

--- image-processing-with-numpy/image-processing.py
+++ image-processing-with-numpy/image-processing.py
@@ -1,13 +1,1 @@
-from PIL import Image
-import numpy as np
-
-img = np.array(Image.open("image/GTA VI(original).jpg"))
-
-def inspect_image():
-    print("Image Properties:")
-    h, w, c = img.shape
-    print(f"Dimension: {img.ndim}D")
-    print(f"Resolution: {w}x{h}")
-    print(f"Channels: {c}")
-
-inspect_image()
+from PIL import Imageimport numpy as npimg = np.array(Image.open("image/GTA VI(original).jpg"))print(img)def inspect_image():    print("Image Properties:")    h, w, c = img.shape    print(f"Dimension: {img.ndim}D")    print(f"Resolution: {w}x{h}")    print(f"Channels: {c}")def pixel_analysis():    print("pixel analysis:")    print("Data Type:", img.dtype)    print("Minimum pixel value:", np.min(img))    print("Maximum pixel value:", np.max(img))    print("Average pixel value:", np.round(np.mean(img), 2))    px = img[:1080, :1920]    print(f"px: {px}")    Image.fromarray(px).show()# def grayscale():#     gray = img.reshape(-1, 3)#     print(gray)#     plt.imsave("image/grayscale.png", gray)inspect_image()pixel_analysis()# arr = img.reshape()# print(arr)
